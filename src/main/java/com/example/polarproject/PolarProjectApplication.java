8491bd94584c364e4d63b15664f8f73ef53a065a
ldeeley <lester.deeley@yahoo.com>
2023-02-27 09:00:53 +0000

    Initial commit

---
+++ src/main/java/com/example/polarproject/PolarProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.polarproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/polarproject/PolarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polarproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
